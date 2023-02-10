4a92b7f6d90a926735343cf2d21846134beda9f4
Senthil Kumar D <65242964+dsenthil23@users.noreply.github.com>
2023-02-10 14:02:12 +0530

    Create ExcelReader.java

---
+++ ExcelReader.java
@@ -0,0 +1,48 @@
+package com.aws.bluage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelReader {
+	
+	public String[][] getExcelData(String fileName, String sheetName) throws IOException {
+        String[][] data = null;
+        try {
+             
+            FileInputStream fis = new FileInputStream(fileName);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheet(sheetName);
+            XSSFRow row = sheet.getRow(0);
+            int noOfRows = sheet.getPhysicalNumberOfRows();
+            int noOfCols = row.getLastCellNum();
+            Cell cell;
+            data = new String[noOfRows - 1][noOfCols];
+            for (int i = 1; i < noOfRows; i++) {
+                for (int j = 0; j < noOfCols; j++) {
+                    row = sheet.getRow(i);
+                    cell = row.getCell(j);
+                    data[i - 1][j] = cell.getStringCellValue();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("The exception is: " + e.getMessage());
+        }
+        return data;
+    }
+}
